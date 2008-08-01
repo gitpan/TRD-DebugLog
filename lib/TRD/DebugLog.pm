993a9f7a39b524047d67ce7447142b0a3fa5ad29
Takuya Ichikawa <ichi@cpan.org>
2008-08-01 06:27:47 +0000

    Import of ICHI/TRD-DebugLog-0.0.5 from CPAN.
    gitpan-cpan-distribution: TRD-DebugLog
    gitpan-cpan-version:      0.0.5
    gitpan-cpan-path:         ICHI/TRD-DebugLog-0.0.5.tar.gz
    gitpan-cpan-author:       ICHI
    gitpan-cpan-maturity:     released

--- lib/TRD/DebugLog.pm
+++ lib/TRD/DebugLog.pm
@@ -1,21 +1,55 @@
 package TRD::DebugLog;
 
-#use warnings;
+use warnings;
 use strict;
-use Carp;
-require Exporter;
 
-our @ISA = qw(Exporter);
-our @EXPORT = qw(
-	dlog
-);
+=head1 NAME
 
-use version;
-our $VERSION = qv('0.0.4');
+TRD::DebugLog - debug log
 
+=head1 VERSION
+
+Version 0.0.5
+
+=cut
+
+our $VERSION = '0.0.5';
 our $enabled = 0;
 our $timestamp = 1;
 our $file = undef;
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use TRD::DebugLog;
+
+    $TRD::DebugLog::enabled = 1;
+    dlog( "this is debug log" );
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 dlog( log )
+
+   show debug log.
+
+   $TRD::DebugLog::enabled
+       = 1 : enable debug log
+       = 0 : disable debug log
+   $TRD::DebugLog::timestamp
+       = 1 : show timestamp enable
+       = 0 : show timestamp disable
+   $TRD::DebugLog::file
+       debug log append to file
+
+=cut
 
 #======================================================================
 sub dlog {
@@ -48,159 +82,60 @@ sub dlog {
 	}
 }
 
-1; # Magic true value required at end of module
-__END__
+=head1 AUTHOR
 
-=head1 NAME
+Takuya Ichikawa, C<< <trd.ichi at gmail.com> >>
 
-TRD::DebugLog - debug log
+=head1 BUGS
 
-
-=head1 VERSION
-
-This document describes TRD::DebugLog version 0.0.1
+Please report any bugs or feature requests to C<bug-trd-debuglog at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=TRD-DebugLog>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
 
 
-=head1 SYNOPSIS
-
-    use TRD::DebugLog;
-    $TRD::DebugLog::enabled = 1;
-
-    dlog( "this is debug log" );
-
-=for author to fill in:
-    Brief code example(s) here showing commonest usage(s).
-    This section will be as far as many users bother reading
-    so make it as educational and exeplary as possible.
-  
-  
-=head1 DESCRIPTION
-
-=for author to fill in:
-    Write a full description of the module and its features here.
-    Use subsections (=head2, =head3) as appropriate.
 
 
-=head1 INTERFACE 
+=head1 SUPPORT
 
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
+You can find documentation for this module with the perldoc command.
+
+    perldoc TRD::DebugLog
 
 
-=head1 DIAGNOSTICS
+You can also look for information at:
 
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
+=over 4
 
-=over
+=item * RT: CPAN's request tracker
 
-=item dlog
-print debug log
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=TRD-DebugLog>
 
-=item C<< Error message here, perhaps with %s placeholders >>
+=item * AnnoCPAN: Annotated CPAN documentation
 
-[Description of error here]
+L<http://annocpan.org/dist/TRD-DebugLog>
 
-=item C<< Another error message here >>
+=item * CPAN Ratings
 
-[Description of error here]
+L<http://cpanratings.perl.org/d/TRD-DebugLog>
 
-[Et cetera, et cetera]
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/TRD-DebugLog>
 
 =back
 
 
-=head1 CONFIGURATION AND ENVIRONMENT
-
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
-TRD::DebugLog requires no configuration files or environment variables.
+=head1 ACKNOWLEDGEMENTS
 
 
-=head1 DEPENDENCIES
+=head1 COPYRIGHT & LICENSE
 
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
+Copyright 2008 Takuya Ichikawa, all rights reserved.
 
-None.
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
 
 
-=head1 INCOMPATIBILITIES
+=cut
 
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
-None reported.
-
-
-=head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
-
-No bugs have been reported.
-
-Please report any bugs or feature requests to
-C<bug-trd-debuglog@rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org>.
-
-
-=head1 AUTHOR
-
-Takuya Ichikawa  C<< <trd.ichi@gmail.com> >>
-
-
-=head1 LICENCE AND COPYRIGHT
-
-Copyright (c) 2008, Takuya Ichikawa C<< <trd.ichi@gmail.com> >>. All rights reserved.
-
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
-
-
-=head1 DISCLAIMER OF WARRANTY
-
-BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
-FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
-OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
-PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
-EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
-ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
-YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
-NECESSARY SERVICING, REPAIR, OR CORRECTION.
-
-IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
-WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
-REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
-LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
-OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
-THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
-RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
-FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
-SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGES.
+1; # End of TRD::DebugLog
--- t/00.load.t
+++
@@ -1,7 +0,0 @@
-use Test::More tests => 1;
-
-BEGIN {
-use_ok( 'TRD::DebugLog' );
-}
-
-diag( "Testing TRD::DebugLog $TRD::DebugLog::VERSION" );
